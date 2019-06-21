32468b88026d1673e14e7c09425dd5d020fec914
Spencer White <swhite215@live.com>
2019-06-21 14:21:02 -0400

    added multiple returns

--- fundamentals/hello.go
+++ fundamentals/hello.go
@@ -2,7 +2,12 @@ package main
 
 import "fmt"
 
-// Entry point to the program. When code exits main, the program is over.
+// Entry point to the program, when code exits main, program is over
 func main() {
 	fmt.Println("Hello World!")
+
+	n, err := fmt.Println("Hello", 100, true)
+
+	fmt.Println(n)   // Returns length of arguments added together
+	fmt.Println(err) // Returns error
 }
