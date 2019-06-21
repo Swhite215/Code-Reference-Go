ea847e81d7e58616be86754d84e77569faec8a08
Spencer White <swhite215@live.com>
2019-06-21 14:29:29 -0400

    use of short declaration operator

---
+++ fundamentals/shortDeclarationOperator.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := 42       // Declares and assigns a value
+	y := 100 + 12 // Declares and assigns an expressions result as a value
+
+	fmt.Println(x + y)
+}
