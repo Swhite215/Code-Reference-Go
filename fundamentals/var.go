c2f513a483f25bb05c3a33aba296e2fbd7d618c0
Spencer White <swhite215@live.com>
2019-06-21 14:36:45 -0400

    added zero values examples

--- fundamentals/var.go
+++ fundamentals/var.go
@@ -2,7 +2,11 @@ package main
 
 import "fmt"
 
-var x = 100 // Use var outside function body, program scope
+var x = 100   // Use var outside function body, program scope
+var a int     // Zero value = 0
+var b float32 // Zero value = 0.0
+var c bool    // Zero value = false
+var d string  // Zero value = ""
 
 func main() {
 	y := 43 // Short declaration operator within function body
