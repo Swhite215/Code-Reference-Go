d68d842c5fc65ca02633e2a194a9afba1e44948b
Spencer White <swhite215@live.com>
2019-06-26 11:15:11 -0400

    basic loop examples

---
+++ fundamentals/controlFlow.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	//Iterative - #1
+	for i := 0; i <= 10; i++ {
+		fmt.Println(i);
+	}
+
+	//Iterative - #2
+	i := 0
+	for i <= 3 {
+		fmt.Println(i);
+		i++;
+	}
+
+	//Iterative - #3
+	for {
+        fmt.Println("loop")
+        break
+	}
+	
+}
+
+// Sequential, Iterative, Conditional
