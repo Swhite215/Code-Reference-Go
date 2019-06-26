b727c73ab24f2db978f4254320f50952813e89c1
Spencer White <swhite215@live.com>
2019-06-26 11:43:04 -0400

    added basic conditionals

--- fundamentals/controlFlow.go
+++ fundamentals/controlFlow.go
@@ -27,6 +27,39 @@ func main() {
         fmt.Println("loop")
         break
 	}
+
+	//Conditional - #1
+	a := 2
+	if a == 2 {
+		fmt.Println("A is 2!")
+	}
+
+	//Conditional - #2
+	b := true
+	if b == false {
+		fmt.Println("B is false!");
+	} else {
+		fmt.Println("B is true!")
+	}
+
+	//Conditional - #3
+	c := "Ten"
+	if c == "One" {
+		fmt.Println("B is One!");
+	} else if c == "Two" {
+		fmt.Println("B is Two!")
+	} else {
+		fmt.Println("B is not One or Two!")
+	}
+
+	///Conditional - #4
+	if num := 9; num < 0 {
+        fmt.Println(num, "is negative")
+    } else if num < 10 {
+        fmt.Println(num, "has 1 digit")
+    } else {
+        fmt.Println(num, "has multiple digits")
+    }
 	
 }
 
