cb34c4065a9521f2bcce052bf511abd05c10b81e
Spencer White <swhite215@live.com>
2019-08-07 10:57:50 -0400

    utilities package

---
+++ testing/utilities.go
@@ -0,0 +1,18 @@
+package utilities
+
+
+func Sum(x int, y int) int {
+	return x + y
+}
+
+func Difference(x int, y int) int {
+	return x - y
+}
+
+func Product(x int, y int) int {
+	return x * y
+}
+
+func Quotient(x int, y int) int {
+	return x / y
+}
