fbe5676c4cdd35b7cb5e79423e272172f642e3ab
Spencer White <swhite215@live.com>
2019-08-08 10:40:32 -0400

    panic example

---
+++ fundamentals/panic.go
@@ -0,0 +1,11 @@
+package main
+
+func main() {
+  fmt.Println("I'm First to Print")
+  
+  defer fmt.Println("I Will Print After Panic!")
+  
+  panic("I am the Panic Value!")
+  
+  fmt.Println("Final Print - I'm Skipped")
+}
