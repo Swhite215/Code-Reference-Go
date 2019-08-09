cdb8ea5a1e75eb34ec6370829926388759e3964f
Spencer White <swhite215@live.com>
2019-08-09 09:05:35 -0400

    how arrays and slices relate

--- fundamentals/arrays.go
+++ fundamentals/arrays.go
@@ -19,4 +19,21 @@ func main() {
 	for index, value := range b {
 		fmt.Println(index, value)
 	}
+	
+	// Example where change to slice affects underlying array
+	names := [4]string{
+		"John",
+		"Paul",
+		"George",
+		"Ringo",
+	}
+	fmt.Println(names)
+
+	a := names[0:2]
+	b := names[1:3]
+	fmt.Println(a, b)
+
+	b[0] = "XXX"
+	fmt.Println(a, b)
+	fmt.Println(names)
 }
