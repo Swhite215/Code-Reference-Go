67a466a8eae31095c2af77853938123f58c12823
Spencer White <swhite215@live.com>
2019-08-13 14:44:45 -0400

    example creating stdout and stderr loggers

---
+++ fundamentals/logger.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"log"
+	"os"
+	"fmt"
+)
+
+type Job struct {
+	Command string
+	Logger *log.Logger
+}
+
+func main() {
+
+	fmt.Println(log.Ltime) // 2
+	fmt.Println(log.Ldate) // 1
+	fmt.Println(log.Lmicroseconds) // 4
+	fmt.Println(log.Llongfile) // 8
+	fmt.Println(log.Lshortfile) // 16
+	fmt.Println(log.LUTC) // 32
+	fmt.Println(log.LstdFlags) // 3 - Ldate | Ltime
+
+
+	job := &Job{"Stdout Logger Demo", log.New(os.Stdout, "Tranquility's Genesis: ", 1 | 16)}
+	jobTwo := &Job{"Stderr Logger Demo", log.New(os.Stderr, "Error: ", log.Ltime)}
+	
+	job.Logger.Print("Go to your destiny!")
+	jobTwo.Logger.Print("File not found!")
+}
